7dcb958ea538f11304781cefa9e883d3d8f7930c
Harsha <saidevaharsha07@gmail.com>
2026-05-29 16:52:44 +0530

    Solved problem

--- 01-Core-Revision/Hashing/Practice-Problems/Characterhashing.java
+++ 01-Core-Revision/Hashing/Practice-Problems/Characterhashing.java
@@ -1,17 +1,17 @@
 import java.util.*;
 public class Characterhashing {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String s = sc.next();
-        int hash[] = new int[26];
-        for(int i = 0; i < s.length(); i++) {
-            hash[s.charAt(i) - 'a']++;
+        try (Scanner sc = new Scanner(System.in)) {
+            String s = sc.next();
+            int hash[] = new int[26];
+            for(int i = 0; i < s.length(); i++) {
+                hash[s.charAt(i) - 'a']++;
+            }
+            int q = sc.nextInt();
+            while(q-- > 0) {
+                char c = sc.next().charAt(0);
+                System.out.println("->"+hash[c - 'a']);
+            }
         }
-        int q = sc.nextInt();
-        while(q-- > 0) {
-            char c = sc.next().charAt(0);
-            System.out.println("->"+hash[c - 'a']);
-        }
-        sc.close();
     }
 }
