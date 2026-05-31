12b081f499e2b76054e3d225e5a9eb8e81779235
Harsha <saidevaharsha07@gmail.com>
2026-05-31 22:04:53 +0530

    Completed Prime Number

--- 01-Learn-the-Basics/02-Build-Up-Logical-Thinking/01-Easy-Medium/07-Prime-Number/Main7.java
+++ 01-Learn-the-Basics/02-Build-Up-Logical-Thinking/01-Easy-Medium/07-Prime-Number/Main7.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Main6 {
+public class Main7 {
     public static void main(String[] args) {
         System.out.print("Enter a number: ");
         Scanner sc = new Scanner(System.in);
