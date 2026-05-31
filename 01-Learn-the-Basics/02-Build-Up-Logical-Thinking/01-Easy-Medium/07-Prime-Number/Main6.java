0bdcb906998edf3b4258aaa5ee30ecfe62b305d6
Harsha <saidevaharsha07@gmail.com>
2026-05-31 15:59:21 +0530

    Completed Prime Number

---
+++ 01-Learn-the-Basics/02-Build-Up-Logical-Thinking/01-Easy-Medium/07-Prime-Number/Main6.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main6 {
+    public static void main(String[] args) {
+        System.out.print("Enter a number: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n <= 1) {
+            System.out.print(n + " is not a prime number.");
+            return;
+        } else {
+            for (int i = 2; i * i <= n; i++) {
+                if (n % i == 0) {
+                    System.out.print(n + " is not a prime number.");
+                    return;
+                }
+            }
+            System.out.print(n + " is a prime number.");
+        }
+    }
+}
