4265618e08a4ac6fa2431928efc98a5131213e3e
Harsha <saidevaharsha07@gmail.com>
2026-05-31 15:15:24 +0530

    Completed Reverse a Number

---
+++ 01-Learn-the-Basics/02-Build-Up-Logical-Thinking/01-Easy-Medium/03-Reverse-a-Number/Main3.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main3 {
+    public static void main(String[] args) {
+        System.out.print("Enter a number: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int rev = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            rev = rev * 10 + digit;
+            n /= 10;
+        }
+        System.out.println("Reverse of a Number is:" + rev);
+    }
+}
