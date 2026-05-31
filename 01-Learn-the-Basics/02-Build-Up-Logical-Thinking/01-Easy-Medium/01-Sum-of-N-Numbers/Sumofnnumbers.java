d019fe8dd9bfe3af2ed9d365f5dd27b57d0a9a51
Harsha <saidevaharsha07@gmail.com>
2026-05-31 12:53:34 +0530

    Added Sum of N Numbers and digits solution

--- 01-Learn-the-Basics/02-Build-Up-Logical-Thinking/01-Easy-Medium/01-Sum-of-N-Numbers/Sumofnnumbers.java
+++ 01-Learn-the-Basics/02-Build-Up-Logical-Thinking/01-Easy-Medium/01-Sum-of-N-Numbers/Sumofnnumbers.java
@@ -3,17 +3,14 @@ import java.util.Scanner;
 public class Sumofnnumbers {
     public static void main(String[] args) {
 
-        try (Scanner sc = new Scanner(System.in)) {
+        Scanner sc = new Scanner(System.in);
 
-            System.out.print("Enter a number: ");
-            int n = sc.nextInt();
-
-            int sum = 0;
-            for (int i = 1; i <= n; i++) {
-                sum += i;
-            }
-
-            System.out.println("Sum of first " + n + " natural numbers is: " + sum);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
         }
+        System.out.println("Sum of first " + n + " natural numbers is: " + sum);
     }
 }
