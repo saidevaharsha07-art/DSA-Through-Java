7c7e802a725279724cfc3c06c3df34aa7412139a
Harsha <saidevaharsha07@gmail.com>
2026-06-10 18:51:03 +0530

    Added Pattern 21 Solution

---
+++ 01-Learn-the-Basics/03-Patterns/21-Pattern/Pat21.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Pat21 {
+
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter the number of rows: ");
+            int n = sc.nextInt();
+
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (i == 1 || i == n || j == 1 || j == n) {
+                        System.out.print("*");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println();
+            }
+        }
+    }
+}
