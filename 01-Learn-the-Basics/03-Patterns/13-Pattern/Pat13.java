e7960e6c4df4ed8c9eb9a911767ef7a6ae70f20c
Harsha <saidevaharsha07@gmail.com>
2026-06-10 17:39:24 +0530

    Added Pattern 13 Solution

---
+++ 01-Learn-the-Basics/03-Patterns/13-Pattern/Pat13.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Pat13 {
+
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter the number of rows: ");
+            int n = sc.nextInt();
+            int count = 1;
+
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= i; j++) {
+                    System.out.print(count + " ");
+                    count++;
+                }
+                System.out.println();
+            }
+        }
+    }
+}
