b1c7b8f3e0132c33630d4087a8af17d3f1116394
Harsha <saidevaharsha07@gmail.com>
2026-06-10 15:50:28 +0530

    Added Pattern 3 Solution

--- 01-Learn-the-Basics/03-Patterns/03-Pattern/Pat3.java
+++ 01-Learn-the-Basics/03-Patterns/03-Pattern/Pat3.java
@@ -6,6 +6,7 @@ public class Pat3 {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number of rows: ");
         int n = sc.nextInt();
+
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print(j + " ");
