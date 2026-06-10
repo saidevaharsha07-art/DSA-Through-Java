2f30ff6878cc7d739905e2bbcbc2b5869046f3ab
Harsha <saidevaharsha07@gmail.com>
2026-06-10 15:45:22 +0530

    Added Pattern 1 Solution

--- 01-Learn-the-Basics/03-Patterns/01-Pattern/Pat1.java
+++ 01-Learn-the-Basics/03-Patterns/01-Pattern/Pat1.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 
 public class Pat1 {
-
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
             System.out.print("Enter the number of rows: ");
