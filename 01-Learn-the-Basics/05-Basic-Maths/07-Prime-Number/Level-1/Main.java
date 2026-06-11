33ad1b37606b6198e8782e7fb7cfa2301e5f5b7b
Harsha <saidevaharsha07@gmail.com>
2026-06-11 20:21:26 +0530

    Complete prime Numbers with 3 difficulty levels

---
+++ 01-Learn-the-Basics/05-Basic-Maths/07-Prime-Number/Level-1/Main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+
+        int n = sc.nextInt();
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                count++;
+            }
+        }
+        if (count == 2) {
+            System.out.println("It is a Prime Number.");
+        } else {
+            System.out.println("It is not a Prime Number.");
+        }
+    }
+}
