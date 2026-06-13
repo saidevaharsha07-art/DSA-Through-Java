4faaf95e1a0252f92706a5ef25b63328da0b951e
Harsha <saidevaharsha07@gmail.com>
2026-06-13 15:01:57 +0530

    Added print name N times recursion solution

---
+++ 01-Learn-the-Basics/06-Basic-Recursion/01-Understand-Recursion/Factorial.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Factorial {
+
+    public static void printName(int count, int n) {
+        if (count > n) {
+            return;
+        }
+        System.out.println("Harsha");
+        printName(count + 1, n);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int n = sc.nextInt();
+        printName(1, n);
+        sc.close();
+    }
+}
