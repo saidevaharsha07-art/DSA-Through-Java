e6c193c84d2c46b3e2cef81417da95644cc24c45
Harsha <saidevaharsha07@gmail.com>
2026-06-13 15:15:31 +0530

    Add print name N times recursion solution

--- 01-Learn-the-Basics/06-Basic-Recursion/02-Print-Name-N-Times/Main.java
+++ 01-Learn-the-Basics/06-Basic-Recursion/02-Print-Name-N-Times/Main.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
 
 public class Main {
-    static void printNumbers(int i, int n) {
-        if (i > n) {
+    static void printName(int count, int n) {
+        if (count > n) {
             return;
         }
-        System.out.println(i);
-        printNumbers(i + 1, n);
+        System.out.println("Harsha");
+        printName(count + 1, n);
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter N : ");
         int n = sc.nextInt();
-        printNumbers(1, n);
+        printName(1, n);
     }
 }
