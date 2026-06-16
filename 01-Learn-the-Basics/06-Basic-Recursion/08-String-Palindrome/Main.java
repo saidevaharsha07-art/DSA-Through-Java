48bf936b846040d0b309ce05b340f874d490a858
Harsha <saidevaharsha07@gmail.com>
2026-06-16 10:51:36 +0530

    updated String pallindrome solution

--- 01-Learn-the-Basics/06-Basic-Recursion/08-String-Palindrome/Main.java
+++ 01-Learn-the-Basics/06-Basic-Recursion/08-String-Palindrome/Main.java
@@ -1,27 +1,20 @@
 import java.util.Scanner;
 
 public class Main {
-
     static boolean palindrome(String s, int i) {
-
         if (i >= s.length() / 2) {
             return true;
         }
-
         if (s.charAt(i) != s.charAt(s.length() - i - 1)) {
             return false;
         }
-
         return palindrome(s, i + 1);
     }
 
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Enter String : ");
         String s = sc.next();
-
         if (palindrome(s, 0)) {
             System.out.println("Palindrome");
         } else {
